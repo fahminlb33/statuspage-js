08f46a2127e311c946567f3bfff327fe22f9817d
fahminlb33 <fahminlb33@gmail.com>
2020-09-11 07:47:49 +0700

    Update frontend using Vuejs

--- static/app.js
+++ static/app.js
@@ -3,23 +3,55 @@ const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
 
-axios({
-    url: "data/last_incident.json",
-    responseType: "json"
-}).then(response => {
-    const latestData = response.data;
-    const countDown = new Date(latestData.date).getTime();
-    setInterval(function () {
-        let now = new Date().getTime();
-        let distance = now - countDown;
-    
-        document.getElementById('days').innerText = Math.floor(distance / (day));
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour));
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute));
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-        document.getElementById('latest_incident').innerText = latestData.incident;
-    }, second);
-})
-.catch(error => {
-    alert('Can not get latest data.');
+var app = new Vue({
+    el: '#app',
+    data: {
+        clock: null,
+        date: null,
+        incident: null,
+        services: null,
+        updateTimer: null
+    },
+    methods: {
+        updateData() {
+            axios({
+                url: "data/last_incident.json",
+                responseType: "json"
+            }).then(response => {
+                const latestData = response.data;
+                this.date = new Date(latestData.date);
+                this.incident = latestData.incident;
+                this.services = latestData.services;
+
+                this.updateTimer = setInterval(() => {
+                    let now = new Date().getTime();
+                    let distance = now - this.date;
+                    this.clock = prettyClock(distance);
+                }, 1000);
+            });
+        },
+
+        statusToMessage(status) {
+            return status === "ok" ? "Service is up" : "Service is down";
+        },
+
+        prettyDate(date) {
+            return new Date(date).toLocaleString();
+        }
+    },
+    created () {
+        this.updateData();
+    },
+    beforeDestroy () {
+        clearInterval(this.updateTimer)
+    },
 });
+
+function prettyClock(distance) {
+    return {
+        days:  Math.floor(distance / (day)),
+        hours: Math.floor((distance % (day)) / (hour)),
+        minutes: Math.floor((distance % (hour)) / (minute)),
+        seconds: Math.floor((distance % (minute)) / second)
+    };
+}
