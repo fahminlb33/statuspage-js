cbfb2ca1facaebf192861cfc6d55b40aed21d512
fahminlb33 <fahminlb33@gmail.com>
2020-09-11 07:47:34 +0700

    Update backend service to include logs

--- app/index.js
+++ app/index.js
@@ -1,56 +1,32 @@
 const fs = require('fs');
-const { exit } = require('process');
 const axios = require('axios').default;
 
-const monitoredUrls = [{
-        uri: "https://gitlab.playcourt.id",
-        desc: "Gitlab"
-    },
-    {
-        uri: "https://dev-minio.logeetrans.com",
-        desc: "logee-minio"
-    },
-    {
-        uri: "https://dev-api.logeetrans.com/user",
-        desc: "logee-user"
-    },
-    {
-        uri: "https://dev-api.logeetrans.com/order",
-        desc: "logee-order"
-    },
-    {
-        uri: "https://dev-api.logeetrans.com/truck",
-        desc: "logee-truck"
-    },
-    {
-        uri: "https://dev-api.logeetrans.com/cargo",
-        desc: "logee-cargo"
-    },
-    {
-        uri: "https://dev-api.logeetrans.com/billing",
-        desc: "logee-billing"
-    }
-];
-
-async function asyncForEach(array, callback) {
-    for (let index = 0; index < array.length; index++) {
-        await callback(array[index], index, array);
-    }
-}
+// configs
+const config = Object.freeze({
+    maxIncidents: 10,
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.44'
+});
 
 const main = async () => {
     const checkResult = [];
-    await asyncForEach(monitoredUrls, async (item) => {
+    const monitoredUrls = JSON.parse(fs.readFileSync('data/monitored.json', 'utf-8'));
+   
+    // run request
+    for (let i = 0; i < monitoredUrls.length; i++)
+    {
+        const item = monitoredUrls[i];
+
         try {
             const opt = {
                 url: item.uri, 
                 responseType: 'text',
                 transformResponse: [],
                 headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.44'
+                    'User-Agent': config.userAgent
                 }
             };
 
+            console.debug(`Checking ${item.desc}, env: ${item.env}`);
             const response = await axios(opt);
             if (!response.data || response.data.includes("Application is not available")) {
                 checkResult.push({
@@ -72,38 +48,77 @@ const main = async () => {
                 date: new Date().toISOString()
             });
         }
-    });
+    }
 
-    const lastIncidentLine = fs.readFileSync('data/last_incident.json', 'utf-8');
-    const lastIncident =
-        lastIncidentLine ?
-        JSON.parse(lastIncidentLine) : {
+    // load latest data
+    const lastIncidentData = fs.readFileSync('data/last_incident.json', 'utf-8');
+    let lastIncident;
+    if (lastIncidentData)
+    {
+        lastIncident = JSON.parse(lastIncidentData);
+    }
+    else
+    {
+        lastIncident = {
             date: new Date().toISOString(),
             incident: "All monitored service is up.",
-            status: "ok"
+            status: "ok",
+            services: []
         };
-
-    const allOk = checkResult.every(x => x.status === "ok");
-    if (!allOk) {
-        if (lastIncident.status == "ok") {
-            const repos = checkResult.filter(x => x.status === "error").map(y => y.desc);
-            const data = {
-                date: new Date().toISOString(),
-                incident: `${repos.join(',')} is down.`,
-                "status": "error"
-            }
-            fs.writeFileSync('data/last_incident.json', JSON.stringify(data))
-        }
-    } else {
-        if (lastIncident.status == "error") {
-            const data = {
-                date: new Date().toISOString(),
-                incident: `All monitored service is up.`,
-                "status": "ok"
-            }
-            fs.writeFileSync('data/last_incident.json', JSON.stringify(data))
-        }
     }
+
+    // save global result
+    const allOk = checkResult.every(x => x.status === "ok");
+    if (!allOk && lastIncident.status == "ok") {
+        const repos = checkResult.filter(x => x.status === "error").map(y => `${y.desc}:${y.env}`);
+        lastIncident.date = new Date().toISOString();
+        lastIncident.incident = `${repos.join(', ')} is down.`;
+        lastIncident.status = "error";
+    } else if (allOk && lastIncident.status == "error") {
+        lastIncident.date = new Date().toISOString();
+        lastIncident.incident = `All monitored service is up.`;
+        lastIncident.status = "ok";
+    }
+
+    // save services status
+    checkResult.forEach(item => {
+        const currentServiceIndex = lastIncident.services.findIndex(x => x.desc == item.desc && x.env == item.env);
+        const currentService = lastIncident.services[currentServiceIndex];
+        if (!currentService) 
+        {
+            lastIncident.services.push({
+                desc: item.desc,
+                env: item.env,
+                status: item.status,
+                incidents: [
+                    {
+                        date: item.date,
+                        status: item.status
+                    }
+                ]
+            });
+        }
+        else
+        {
+            const currentServiceLastIncident = currentService.incidents[currentService.incidents.length - 1];
+            if (currentServiceLastIncident.status !== item.status)
+            {
+                if (lastIncident.services[currentServiceIndex].incidents.length >= config.maxIncidents) 
+                {
+                    lastIncident.services[currentServiceIndex].incidents.shift();
+                }
+
+                lastIncident.services[currentServiceIndex].incidents.push({
+                    date: item.date,
+                    status: item.status
+                });
+            };
+        }
+    });
+
+    // save updated data
+    fs.writeFileSync('data/last_incident.json', JSON.stringify(lastIncident, null, 2));
 }
 
-main();
+// bootstrap
+main().catch(error => console.error(error));
